cd623dd8d0f77c18f8de26817c2e8cd46491db7b
amanpandey3956 <amanpandey39563@gmail.com>
2024-08-26 12:37:56 +0530

    updated

--- AppwriteBlog/src/components/post-form/PostForm.jsx
+++ AppwriteBlog/src/components/post-form/PostForm.jsx
@@ -20,34 +20,37 @@ export default function PostForm({ post }) {
 
     const submit = async (data) => {
         if (post) {
-            const file = data.image[0] ? await appwriteService.uploadFile(data.image[0]) : null;
-
-            if (file) {
-                appwriteService.deleteFile(post.featuredImage);
-            }
-
-            const dbPost = await appwriteService.updatePost(post.$id, {
-                ...data,
-                featuredImage: file ? file.$id : undefined,
-            });
-
-            if (dbPost) {
-                navigate(`/post/${dbPost.$id}`);
-            }
+          const file = data.image[0] ? await appwriteService.uploadFile(data.image[0]) : null;
+      
+          if (file) {
+            appwriteService.deleteFile(post.featuredImage);
+          }
+      
+          const dbPost = await appwriteService.updatePost(post?.$id, {
+            ...data,
+            featuredImage: file ? file.$id : undefined,
+          });
+      
+          if (dbPost) {
+            navigate(`/post/${dbPost.$id}`);
+          }
         } else {
-            const file = await appwriteService.uploadFile(data.image[0]);
-
-            if (file) {
-                const fileId = file.$id;
-                data.featuredImage = fileId;
-                const dbPost = await appwriteService.createPost({ ...data, userId: userData.$id });
-
-                if (dbPost) {
-                    navigate(`/post/${dbPost.$id}`);
-                }
+          const file = await appwriteService.uploadFile(data.image[0]);
+      
+          if (file) {
+            const fileId = file.$id;
+            data.featuredImage = fileId;
+            const dbPost = await appwriteService.createPost({
+              ...data,
+              userId: userData?.$id, // Add null check here
+            });
+      
+            if (dbPost) {
+              navigate(`/post/${dbPost.$id}`);
             }
+          }
         }
-    };
+      };
 
     const slugTransform = useCallback((value) => {
         if (value && typeof value === "string")
